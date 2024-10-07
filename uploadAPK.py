53b9c26554ed14ca86471addab2914c633794157
Liruochen1207 <liruochen01@outlook.com>
2024-10-07 21:19:39 +0800

    test

--- uploadAPK.py
+++ uploadAPK.py
@@ -1,15 +1,15 @@
-import base64
-import requests
+# import base64
+# import requests
 
-byte = None
+# byte = None
 
-with open(r"build\app\outputs\flutter-apk\app-release.apk", "rb") as f:
-    byte = f.read()
+# with open(r"build\app\outputs\flutter-apk\app-release.apk", "rb") as f:
+#     byte = f.read()
 
-version = "1.0.2"
+# version = "1.0.2"
 
 
-content = base64.b64encode(byte).decode()
-po = requests.post("http://47.108.91.180:5000/upload_apk", data = {"name":"base", "version":version, "content":content }).content.decode('utf-8')
+# content = base64.b64encode(byte).decode()
+# po = requests.post("http://47.108.91.180:5000/upload_apk", data = {"name":"base", "version":version, "content":content }).content.decode('utf-8')
     
-print(po)
+# print(po)
